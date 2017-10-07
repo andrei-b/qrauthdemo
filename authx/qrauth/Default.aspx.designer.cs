3100e44ce304e4c304bc7b3a13b45c48ad649318
Andrei <andrei.borovsky@gmail.com>
2017-10-07 18:24:18 +0300

    Revert "added some log"
    This reverts commit e7ab54e2e38f3238249b8490145fdd9a3505c748.

--- authx/qrauth/Default.aspx.designer.cs
+++ authx/qrauth/Default.aspx.designer.cs
@@ -22,12 +22,12 @@ namespace qrauth {
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
         
         /// <summary>
-        /// LoginView1 control.
+        /// HyperLink1 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.LoginView LoginView1;
+        protected global::System.Web.UI.WebControls.HyperLink HyperLink1;
     }
 }
